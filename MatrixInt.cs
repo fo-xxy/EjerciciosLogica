7d3ce91fb4b5fe4013ca4f34c534ddd237681345
fo-xxy <robert.yarce.12@hotmail.com>
2025-06-01 14:46:43 -0500

    .

---
+++ MatrixInt.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosLogica
+{
+    public class MatrixInt
+    {
+        //Se crea el objeto
+        private object MyMatrix;
+
+        //Se crea el construtor de la clase
+        public  MatrixInt(object matrix)
+        {
+            this.MyMatrix = matrix;
+
+            dimension();
+        }
+
+        #region Metodos
+
+        //Metodo que nos devuelve la dimensión
+        public int dimension()
+        {
+           return getDimension(MyMatrix);
+        }
+
+        //En este método hayamos la dimensión de la matriz, donde nos retornará el resultado en entero.
+        private int getDimension(object obj)
+        {
+            if (obj is IList list && list.Count > 0)
+            {
+                return 1 + getDimension(list[0]);
+            }
+            return 0;
+        }
+
+
+        public bool straight()
+        {
+            return false;
+        }
+
+        public int compute()
+        {
+            return 0;
+        }
+
+        #endregion
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EjerciciosLogica
+{
+    internal class Program
+    {
+        public MatrixInt matrixInt;
+        //public string obj = "[[[1, 2,3]], [[5, 6, 7],[5, 4, 3]], [[3, 5, 6], [4, 8, 3], [2, 3]]]";
+        //public string obj = " [[[1, 2, 3], [2, 3, 4]], [[5, 6, 7], [5, 4, 3]], [[3,5, 6], [4, 8, 3]]]";
+        //public string obj = "  [1,2]";
+
+
+        //public string obj = "[1, 2]";
+        //public string obj = "[[1, 2], [2, 4]]";
+        //public string obj = "[[1, 2],[2, 4],[2, 4]]";
+        //public string obj ="[[[3, 4],[6, 5]]]" ;
+        //public string obj ="[[[1, 2, 3]], [[5, 6, 7], [5, 4, 3]], [[3, 5, 6], [4, 8, 3], [2, 3]]] ";
+        public string objMatrix ="[[[1, 2, 3], [2, 3, 4]], [[5, 6, 7], [5, 4, 3]], [[3, 5, 6], [4, 8, 3]]]";
+
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            p.ObtenerDimension();
+        }
+
+        public void ObtenerDimension()
+        {
+            // Esta línea valida que todas las matrices estén bien formadas, asegurando que por cada '[' haya un ']' correspondiente.
+            // asi extrae únicamente las partes válidas del texto para procesarlas correctamente.
+            var matches = Regex.Matches(objMatrix, @"\[\s*(?:\[(?:\[(?:[^\[\]]|(?<open>\[)|(?<-open>\]))*\]|[^\[\]]*)\]\s*,?\s*)+\](?(open)(?!))");
+
+            try
+            {
+                // Intentar parsear directamente el string completo
+                var jsonFormateado = JsonConvert.DeserializeObject<object>(objMatrix);
+
+                //Pasamos el objeto al constructor de la clase MatrixInt
+                matrixInt = new MatrixInt(jsonFormateado);
+
+                //Asignamos el resultado a una variable local para obtener la dimensión
+                int dim = matrixInt.dimension();
+
+                //Imprimimos el resultado.
+                Console.WriteLine($"Dimensión: {dim}");
+            }
+            catch (Exception ex)
+            {
+                //Si llega a salir un error nos mostrará este mensaje con el error.
+                Console.WriteLine("Error al parsear la matriz completa.");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
